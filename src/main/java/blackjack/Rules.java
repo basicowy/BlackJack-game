26c7da79ce472631240fc517a3c8d8811896cce3
wifoon <ikazior@icloud.com>
2026-06-03 21:37:03 +0200

    uzupelnienie funkcjonalnosci metody

--- src/main/java/blackjack/Rules.java
+++ src/main/java/blackjack/Rules.java
@@ -2,6 +2,15 @@ package blackjack;
 
 public class Rules {
     public static RoundResult determineWinner(int playerScore, int dealerScore, boolean isPlayerBust, boolean isDealerBust) {
-        return null;
+        if (isPlayerBust) return RoundResult.PLAYER_BUST;
+
+        if (isDealerBust) return RoundResult.PLAYER_WIN;
+
+        if (playerScore == 21) return RoundResult.BLACKJACK_WIN;
+
+        if (playerScore == dealerScore) return RoundResult.PUSH;
+
+        if (playerScore <= 21 && playerScore > dealerScore) return RoundResult.PLAYER_WIN;
+        else return RoundResult.DEALER_WIN;
     }
 }
