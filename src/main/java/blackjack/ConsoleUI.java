048c4df716269044690825bff0d63aef5ad2a9ff
Bartek <xenn01011@gmail.com>
2026-06-08 08:35:19 +0200

    dodanie klas ConsoleUI i InputHandler wraz z testami wejscia

---
+++ src/main/java/blackjack/ConsoleUI.java
@@ -0,0 +1,11 @@
+package blackjack;
+
+public class ConsoleUI {
+    public void displayWelcome() {
+        System.out.println("Welcome to Blackjack!");
+    }
+    public void displayMessage(String msg) {
+        System.out.println(msg);
+    }
+}
+
